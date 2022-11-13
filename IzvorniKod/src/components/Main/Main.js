1b0b3d926aa7a3c62844a7a5a3e03fd9e0a08edd
Dominik Juršić <dominik.jursic@gmail.com>
2022-11-13 23:41:06 +0100

    deploy server

--- IzvorniKod/src/components/Main/Main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { MainComponent } from "./MainStyle";
-
-const Main = (props) => {
-  return <MainComponent>{props.children}</MainComponent>;
-};
-
-export default Main;
--- IzvorniKod/src/components/Main/MainStyle.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const MainComponent = styled.main`
-  width: 100%;
-  min-height: 80vh;
-  overflow: hidden;
-  background-color: #EAE7DC;
-  padding-bottom: 40px;
-`;
